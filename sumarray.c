27a10bf698417048158deb170457375b32aac881
Sanyogeeta Patil <sanyogeetapatil95@gmail.com>
2026-07-15 21:11:11 +0530

    Add files via upload

---
+++ sumarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+  int a[10],sum=0,i;
+  printf("enter 10 elements in array");
+   for(i=0;i<10;i++)
+  {
+   scanf("%d",&a[i]);
+  }
+  for(i=0;i<10;i++)
+   {
+    sum=sum+a[i];
+   }
+  
+  printf("sum of in array elements=%d",sum);
+  return 0 ;
+
+}
